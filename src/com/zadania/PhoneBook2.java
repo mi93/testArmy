bf7cd79eb27b383c0cab77b2a9be3f35de98c954
Kinga Michniak <kinga.michniak@outlook.com>
2022-03-31 21:07:30 +0200

    Another version of the homework ('PhoneBook 2) and small correction in the previous one (PhoneBook 1)

--- src/com/zadania/PhoneBook2.java
+++ src/com/zadania/PhoneBook2.java
@@ -1,12 +1,12 @@
 package com.zadania;
 
-public class PhoneBook {
+public class PhoneBook2 {
     public static void main(String[] args)
     {
 
         int id = 13;
         String name = "Bono";
-        String surname = "Bo";
+        String surname = "";
         int phoneNumber = 333999666;
         int age = 20;
         String contact = "";
@@ -20,25 +20,50 @@ public class PhoneBook {
 
     public static void printContactCard(int id, String name, String surname, int age, int phoneNumber)
     {
+
+        if (name == "") {
+            name = "Unknown";
+        }
+        else if (surname == "") {
+            surname = "Unknown";
+        }
         System.out.println("Id: " + id + "\nName: " + name + "\nSurname: " + surname + "\nage: " + age + "\nnumber: " + phoneNumber);
     }
 
     public static void printContact (int id, String name, String surname, int phoneNumber, int age)
     {
+
+        if (name == "") {
+            name = "Unknown";
+        }
+        else if (surname == "") {
+            surname = "Unknown";
+        }
         System.out.println(id + " | " + name + " | " + surname + " | " + age + " | " + phoneNumber);
     }
 
     public static String getContact(int id, String name, String surname, int age, int phoneNumber)
     {
+        if (name == "") {
+            name = "Unknown";
+        }
+        else if (surname == "") {
+            surname = "Unknown";
+        }
         return (id + " | " + name + " | " + surname + " | " + age + " | " + phoneNumber);
     }
 
     public static String getContactRaw(int id, String name, String surname, int age, int phoneNumber, boolean isMobile)
     {
+        if (name == "") {
+            name = "Unknown";
+        }
+        else if (surname == "") {
+            surname = "Unknown";
+        }
         return (id + " | " + name + " | " + surname + " | " + age + " | " + phoneNumber + " | " + isMobile );
     }
 }
-
 
 
 
