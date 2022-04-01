c87922aa5958cd80801d9993fb128d0a9a6ad009
Kinga Michniak <kinga.michniak@outlook.com>
2022-04-01 11:47:10 +0200

    Adding phoneNumber functionality to PhoneBook2 file

--- src/com/zadania/PhoneBook2.java
+++ src/com/zadania/PhoneBook2.java
@@ -7,63 +7,88 @@ public class PhoneBook2 {
         int id = 13;
         String name = "Bono";
         String surname = "";
-        int phoneNumber = 333999666;
+        String phoneNumber = "3339996";
         int age = 20;
         String contact = "";
         boolean isHidden = false;
 
         printContactCard(id, name, surname, age, phoneNumber);
         printContact(id, name, surname, phoneNumber, age);
-        System.out.println(getContact(id, name, surname, phoneNumber, age));
+        System.out.println(getContact(id, name, surname, age, phoneNumber));
         System.out.println(getContactRaw(id, name, surname, age, phoneNumber, isHidden));
     }
 
-    public static void printContactCard(int id, String name, String surname, int age, int phoneNumber)
+    public static void printContactCard(int id, String name, String surname, int age, String phoneNumber)
     {
 
-        if (name == "") {
+        if (name == "")
+        {
             name = "Unknown";
         }
-        else if (surname == "") {
+        if (surname == "")
+        {
             surname = "Unknown";
         }
+        if (phoneNumber.length() != 9)
+        {
+            phoneNumber = "000000000";
+        }
+
         System.out.println("Id: " + id + "\nName: " + name + "\nSurname: " + surname + "\nage: " + age + "\nnumber: " + phoneNumber);
     }
 
-    public static void printContact (int id, String name, String surname, int phoneNumber, int age)
+    public static void printContact (int id, String name, String surname, String phoneNumber, int age)
     {
 
-        if (name == "") {
+        if (name == "")
+        {
             name = "Unknown";
         }
-        else if (surname == "") {
+        if (surname == "")
+        {
             surname = "Unknown";
+        }
+        if (phoneNumber.length() != 9)
+        {
+            phoneNumber = "000000000";
         }
         System.out.println(id + " | " + name + " | " + surname + " | " + age + " | " + phoneNumber);
     }
 
-    public static String getContact(int id, String name, String surname, int age, int phoneNumber)
+    public static String getContact(int id, String name, String surname, int age, String phoneNumber)
     {
-        if (name == "") {
+        if (name == "")
+        {
             name = "Unknown";
         }
-        else if (surname == "") {
+        if (surname == "")
+        {
             surname = "Unknown";
+        }
+        if (phoneNumber.length() != 9)
+        {
+            phoneNumber = "000000000";
         }
         return (id + " | " + name + " | " + surname + " | " + age + " | " + phoneNumber);
     }
 
-    public static String getContactRaw(int id, String name, String surname, int age, int phoneNumber, boolean isMobile)
+    public static String getContactRaw(int id, String name, String surname, int age, String phoneNumber, boolean isMobile)
     {
-        if (name == "") {
+        if (name == "")
+        {
             name = "Unknown";
         }
-        else if (surname == "") {
+        if (surname == "")
+        {
             surname = "Unknown";
         }
+        if (phoneNumber.length() != 9)
+        {
+            phoneNumber = "000000000";
+        }
+
         return (id + " | " + name + " | " + surname + " | " + age + " | " + phoneNumber + " | " + isMobile );
     }
 }
-
 
 
