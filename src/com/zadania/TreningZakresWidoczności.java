61833be71fb87518c997ad66a1764c20be648be5
Kinga Michniak <kinga.michniak@outlook.com>
2022-04-04 14:50:53 +0200

    Some corrections

---
+++ src/com/zadania/TreningZakresWidoczności.java
@@ -0,0 +1,29 @@
+package com.zadania;
+
+public class TreningZakresWidoczności {
+
+
+    public static void main(String[] args) {
+        int liczba1 = 2;
+        int liczba2 = 2;
+        int suma = liczba1 + liczba2;
+        System.out.println();
+
+//        public void wypiszCoś() {
+//            System.out.println("Wypisuje sume" + suma);
+//         Java does not allows defining a method inside another method.
+//         Attempting to do that would throw the Illegal start of expression error.
+
+        }
+//        public void wypiszSume() {
+//            System.out.println("Wypisuje sume" + suma);
+//
+//            for(int i=0; i<5; i++){
+//                System.out.println("Wypisuję sumę po raz: " + i);
+//            }
+//        wypiszSume();
+//    }
+
+}
+
+
