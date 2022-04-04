b413cbd35fb354972f9ebc898cccbf8924579b40
Kinga Michniak <kinga.michniak@outlook.com>
2022-04-04 14:56:24 +0200

    Merging createTable class into Phonebook2

--- src/com/zadania/PhoneBook2.java
+++ src/com/zadania/PhoneBook2.java
@@ -16,6 +16,15 @@ public class PhoneBook2 {
         printContact(id, name, surname, phoneNumber, age);
         System.out.println(getContact(id, name, surname, age, phoneNumber));
         System.out.println(getContactRaw(id, name, surname, age, phoneNumber, isHidden));
+
+        int tab[] = new int[10];
+        System.out.println("Value of index 0: " + getLastDialed(tab));
+        System.out.println("Value of index provided by the user: " + getIndex(tab, 4));
+        dial(tab, 2);
+        System.out.println("Value of the first index check: " + tab[0]);
+        removeLastDialed(tab, 5);
+        System.out.println("Value of the 5th index check: " + tab[5]);
+
     }
 
     public static void printContactCard(int id, String name, String surname, int age, String phoneNumber)
@@ -89,4 +98,29 @@ public class PhoneBook2 {
 
         return (id + " | " + name + " | " + surname + " | " + age + " | " + phoneNumber + " | " + isMobile );
     }
+
+    public static int getLastDialed(int[] tab)
+    {
+        return tab[0];
+    }
+
+    public static int getIndex(int[] tab, int index)
+    {
+        if (tab[index - 1] == 0)
+        {
+            System.out.println("Given index contains incorrect id");
+        }
+        return tab[index - 1];
+    }
+
+    public static int dial(int[] tab, int id)
+    {
+        return tab[0] = id;
+    }
+
+    public static int removeLastDialed(int[] tab, int index)
+    {
+        return tab[index - 1] = 0;
+    }
+
 }
